89042ced29a22983f52cb4be388c7eb290380a5a
YeFei572 <572yefei@163.com>
2021-06-22 16:44:38 +0800

    update light and dark themes

--- lib/pages/alerts/alerts_controller.dart
+++ lib/pages/alerts/alerts_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AlertsController extends GetxController {
@@ -5,11 +6,35 @@ class AlertsController extends GetxController {
 
   String keyWord = "";
 
-
+  final int DEFAULT_SCROLLER = 300;
+  final int DEFAULT_SHOW_TOP_BTN = 1000;
+  ScrollController scrollController = ScrollController();
+  RxDouble toolbarOpacity = 0.0.obs;
+  bool showToTopBtn = false;
 
   @override
   void onInit() {
     super.onInit();
+    print("start ....");
+    if (scrollController.hasClients) {
+      scrollController.addListener(() {
+        print("-------------${scrollController.offset}");
+        double t = scrollController.offset / DEFAULT_SCROLLER;
+        if (t < 0.0) {
+          t = 0.0;
+        } else if (t > 1.0) {
+          t = 1.0;
+        }
+        toolbarOpacity.value = t;
+
+        if(scrollController.offset < DEFAULT_SHOW_TOP_BTN && showToTopBtn){
+          showToTopBtn = false;
+        }else if(scrollController.offset >= DEFAULT_SHOW_TOP_BTN && !showToTopBtn){
+          showToTopBtn = true;
+        }
+      });
+     print(scrollController.offset);
+    }
 
   }
 }
--- lib/pages/alerts/alerts_page.dart
+++ lib/pages/alerts/alerts_page.dart
@@ -1,5 +1,4 @@
 import 'package:demo01/pages/alerts/alerts_controller.dart';
-import 'package:demo01/pages/components/loading_overlay.dart';
 import 'package:demo01/routes/app_routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,75 +7,132 @@ import 'package:get/get.dart';
 
 class AlertsPage extends GetView<AlertsController> {
   Widget build(BuildContext context) {
-    ThemeData currTheme = Theme.of(context);
     return GetBuilder<AlertsController>(
       builder: (controller) => Scaffold(
-        appBar: AppBar(
-          title: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: 55.w,
-              maxHeight: 55.w,
-              minWidth: 700.w,
-              maxWidth: 700.w,
-            ),
-            child: Form(
-              child: TextFormField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 0),
-                  hintText: "请输入关键词",
-                  fillColor: Color(0xffF4F6F8),
-                  filled: true,
-                  hintStyle: TextStyle(fontSize: 24.sp,color: Colors.black54),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.w),
-                    borderSide: BorderSide(color: Colors.pinkAccent),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.w),
-                    borderSide: BorderSide(color: Colors.pinkAccent),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.w),
-                    borderSide: BorderSide(color: Colors.grey.withOpacity(0.6)),
-                  ),
-                  prefixIcon: Container(
-                    child: Icon(
-                      CupertinoIcons.search,
-                      color: Colors.grey.withOpacity(0.5),
-                    ),
-                  ),
-                  suffixIcon: GestureDetector(
-                    onTap: () {},
-                    child: Icon(
-                      CupertinoIcons.clear,
-                      color: Colors.grey.withOpacity(0.5),
-                    ),
-                  ),
-                ),
-                onSaved: (value) {
-                  controller.keyWord = value!;
+        body: Stack(
+          children: <Widget>[
+            MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child: ListView.separated(
+                controller: controller.scrollController,
+                itemCount: 200,
+                itemBuilder: (ctx, int index) {
+                  if (index == 0) {
+                    return Container(
+                      height: 300.w,
+                      color: Colors.yellowAccent.withOpacity(0.3),
+                    );
+                  }
+                  return Container(
+                    height: 100.w,
+                    color: Colors.pinkAccent.withOpacity(0.3),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 20.w,
+                    color: Colors.white,
+                  );
                 },
               ),
             ),
-          ),
-        ),
-        body: GetBuilder<AlertsController>(
-          builder: (controller) {
-            return LoadingOverlay(
-              isLoading: controller.loadingFlag,
-              child: Column(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Get.toNamed(AppRoutes.SECOND_PAGE);
-                      },
-                      icon: Icon(
-                        Icons.next_plan_outlined,
-                        color: Colors.pinkAccent,
-                      ))
-                ],
+            Obx(()=>Opacity(
+              opacity: controller.toolbarOpacity.value,
+              child: Container(
+                height: 98.w,
+                color: Colors.blue,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 30.w),
+                  child: Center(
+                    child: Text(
+                      "scroll demo",
+                      style: TextStyle(color: Colors.black, fontSize: 32.sp),
+                    ),
+                  ),
+                ),
               ),
-            );
+            ),)
+          ],
+        ),
+        floatingActionButton: !controller.showToTopBtn
+            ? null
+            : FloatingActionButton(
+                child: Icon(
+                  Icons.keyboard_arrow_up,
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  controller.scrollController.animateTo(0.0,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.ease);
+                },
+              ),
+      ),
+    );
+  }
+
+  List<Widget> _buildActions() {
+    return [
+      TextButton.icon(
+          onPressed: () {
+            Get.toNamed(AppRoutes.SECOND_PAGE);
+          },
+          icon: Icon(
+            Icons.next_plan_outlined,
+            color: Colors.white,
+          ),
+          label: Text(
+            "视频demo",
+            style: TextStyle(color: Colors.white),
+          ))
+    ];
+  }
+
+  Widget _buildSearchInput() {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minHeight: 55.w,
+        maxHeight: 55.w,
+        minWidth: 500.w,
+        maxWidth: 500.w,
+      ),
+      child: Form(
+        child: TextFormField(
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0),
+            hintText: "请输入关键词",
+            fillColor: Color(0xffF4F6F8),
+            filled: true,
+            hintStyle: TextStyle(fontSize: 24.sp, color: Colors.black54),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.w),
+              borderSide: BorderSide(color: Colors.pinkAccent),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.w),
+              borderSide: BorderSide(color: Colors.pinkAccent),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.w),
+              borderSide: BorderSide(color: Colors.grey.withOpacity(0.6)),
+            ),
+            prefixIcon: Container(
+              child: Icon(
+                CupertinoIcons.search,
+                color: Colors.grey.withOpacity(0.5),
+              ),
+            ),
+            suffixIcon: GestureDetector(
+              onTap: () {},
+              child: Icon(
+                CupertinoIcons.clear,
+                color: Colors.grey.withOpacity(0.5),
+              ),
+            ),
+          ),
+          onSaved: (value) {
+            controller.keyWord = value!;
           },
         ),
       ),
